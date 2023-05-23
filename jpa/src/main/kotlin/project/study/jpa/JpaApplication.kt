8c2eab9806c51f9d47623c1e18d34574ed26d071
devjun10 <devjun1023@gmail.com>
2023-05-24 02:17:23 +0900

    feature: add @EnableJpaAuditing, @EnableJpaRepositories annotation
    - JPA Auditing과 Envers를 사용하기 위한 어노테이션 추가

--- jpa/src/main/kotlin/project/study/jpa/JpaApplication.kt
+++ jpa/src/main/kotlin/project/study/jpa/JpaApplication.kt
@@ -2,8 +2,13 @@ package project.study.jpa
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.envers.repository.support.EnversRevisionRepositoryFactoryBean
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
+@EnableJpaAuditing
 @SpringBootApplication
+@EnableJpaRepositories(repositoryFactoryBeanClass = EnversRevisionRepositoryFactoryBean::class)
 class JpaApplication
 
 fun main(args: Array<String>) {
