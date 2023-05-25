26cbba9e29f3729bd48e906176c4667db5b60cd7
devjun10 <devjun1023@gmail.com>
2023-05-25 16:32:55 +0900

    feature: add cache configuration
    - Caffeine 캐시 설정 추가

---
+++ caffeine/src/main/java/project/study/caffeine/common/configuration/CacheConfiguration.java
@@ -0,0 +1,28 @@
+package project.study.caffeine.common.configuration;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static java.util.concurrent.TimeUnit.HOURS;
+
+@EnableCaching
+@Configuration
+public class CacheConfiguration {
+
+    @Bean
+    public Caffeine<Object, Object> caffeineConfig() {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(1, HOURS);
+    }
+
+    @Bean
+    public CacheManager cacheManager(Caffeine<Object, Object> caffeine) {
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+        caffeineCacheManager.setCaffeine(caffeine);
+        return caffeineCacheManager;
+    }
+}
