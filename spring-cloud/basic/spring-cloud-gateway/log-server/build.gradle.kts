ff70ad75ebdbb3a9820c8c7a8cf9e43d3da7c3fa
devjun10 <devjun1023@gmail.com>
2023-06-17 17:26:13 +0900

    chore: add spring-data-relational dependency
    - @Column 어노테이션을 사용하기 위한 SpringData 의존성 추가

--- spring-cloud/basic/spring-cloud-gateway/log-server/build.gradle.kts
+++ spring-cloud/basic/spring-cloud-gateway/log-server/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
     // MongoDB
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation("org.springframework.data:spring-data-relational:3.0.6")
 
     // Kafka
     implementation("org.springframework.kafka:spring-kafka")
