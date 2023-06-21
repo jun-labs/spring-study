9d562f024a3d51f6a18bb4737864907c59a2d8e1
devjun10 <devjun1023@gmail.com>
2023-06-22 04:10:50 +0900

    test: post delete test refactoring
    - 게시글 갯수 세기 케이스 추가

--- domain-event/mono/src/test/kotlin/project/study/event/common/PersistenceHelper.kt
+++ domain-event/mono/src/test/kotlin/project/study/event/common/PersistenceHelper.kt
@@ -4,7 +4,11 @@ import org.springframework.stereotype.Component
 import project.study.event.common.exception.utils.fail
 import project.study.event.core.domain.bookmark.entity.Bookmark
 import project.study.event.core.domain.bookmark.persistence.BookmarkRepository
+import project.study.event.core.domain.comment.entity.Comment
+import project.study.event.core.domain.comment.persistence.CommentRepository
 import project.study.event.core.domain.common.field.Deleted
+import project.study.event.core.domain.notification.entity.Notification
+import project.study.event.core.domain.notification.persistence.NotificationRepository
 import project.study.event.core.domain.post.entity.Post
 import project.study.event.core.domain.post.persistence.PostRepository
 import project.study.event.core.web.post.application.exception.PostNotFoundException
@@ -12,7 +16,9 @@ import project.study.event.core.web.post.application.exception.PostNotFoundExcep
 @Component
 class PersistenceHelper(
     private val postRepository: PostRepository,
-    private val bookmarkRepository: BookmarkRepository
+    private val bookmarkRepository: BookmarkRepository,
+    private val commentRepository: CommentRepository,
+    private val notificationRepository: NotificationRepository
 ) {
 
     fun persist(post: Post): Post {
@@ -31,5 +37,18 @@ class PersistenceHelper(
     fun findBookmarksById(bookmarkId: Long): List<Bookmark> {
         return bookmarkRepository.findAllBookmarksById(bookmarkId)
             .filter { it.delete == Deleted.FALSE }
+    }
+
+    fun persist(comment: Comment): Comment {
+        return commentRepository.save(comment)
+    }
+
+    fun findCommentsById(postId: Long): List<Comment> {
+        return commentRepository.findCommentsById(postId)
+            .filter { it.deleted == Deleted.FALSE }
+    }
+
+    fun findNotificationById(userId: Long): Notification {
+        return notificationRepository.findNotificationById(userId)
     }
 }
--- domain-event/mono/src/test/kotlin/project/study/event/test/post/PostDeleteIntegrationTest.kt
+++ domain-event/mono/src/test/kotlin/project/study/event/test/post/PostDeleteIntegrationTest.kt
@@ -1,4 +1,4 @@
-package project.study.event.test
+package project.study.event.test.post
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import project.study.event.common.PersistenceHelper
 import project.study.event.core.domain.bookmark.entity.Bookmark
+import project.study.event.core.domain.comment.entity.Comment
 import project.study.event.core.domain.post.entity.Post
 import project.study.event.core.web.post.application.PostCommandService
 
@@ -28,13 +29,18 @@ class PostDeleteIntegrationTest {
         postCommandService.publishEvent(newPost.postId!!)
 
         val findBookmarks = persistenceHelper.findBookmarksById(newPost.postId!!)
+        val findComments = persistenceHelper.findCommentsById(newPost.postId!!)
+
         assertEquals(0, findBookmarks.size)
+        assertEquals(0, findComments.size)
     }
 
     private fun given(): Post {
         val newPost = persistenceHelper.persist(Post(null, "Hello", "World"))
         persistenceHelper.persist(Bookmark(null, newPost))
         persistenceHelper.persist(Bookmark(null, newPost))
+        persistenceHelper.persist(Comment(null, 1L, newPost.postId!!, "Hello"))
+        persistenceHelper.persist(Comment(null, 1L, newPost.postId!!, "Hello"))
         return newPost
     }
 }
