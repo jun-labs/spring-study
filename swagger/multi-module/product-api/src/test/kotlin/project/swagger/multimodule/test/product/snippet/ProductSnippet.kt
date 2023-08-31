d59a728b02be1583a89be5bce48a2768b73a713b
devjun10 <devjun1023@gmail.com>
2023-09-01 03:19:22 +0900

    test: update product snippet

--- swagger/multi-module/product-api/src/test/kotlin/project/swagger/multimodule/test/product/snippet/ProductSnippet.kt
+++ swagger/multi-module/product-api/src/test/kotlin/project/swagger/multimodule/test/product/snippet/ProductSnippet.kt
@@ -1,15 +1,24 @@
 package project.swagger.multimodule.test.product.snippet
 
 import com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document
+import com.epages.restdocs.apispec.ParameterDescriptorWithType
 import com.epages.restdocs.apispec.ResourceDocumentation
 import com.epages.restdocs.apispec.ResourceSnippetParameters
+import com.epages.restdocs.apispec.SimpleType
 import org.springframework.restdocs.payload.JsonFieldType
 import org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath
+import org.springframework.restdocs.request.RequestDocumentation
 
 interface ProductSnippet {
 
     companion object {
         private const val IDENTIFIER: String = "{class-name}/{method-name}"
+
+        private val PRODUCT_SEARCH_BY_ID_PATH_PARAMETER: ParameterDescriptorWithType =
+            ParameterDescriptorWithType.fromParameterDescriptor(
+                RequestDocumentation.parameterWithName("id")
+                    .description("Product id.")
+            ).type(SimpleType.NUMBER)
 
         private val PRODUCT_SEARCH_BY_ID_RESPONSE_FIELD_DESCRIPTORS = listOf(
             fieldWithPath("id").type(JsonFieldType.NUMBER)
@@ -26,6 +35,7 @@ interface ProductSnippet {
                         .summary("Product details searching.")
                         .description("API for searching product details by id.")
                         .tags("Product")
+                        .pathParameters(PRODUCT_SEARCH_BY_ID_PATH_PARAMETER)
                         .responseFields(PRODUCT_SEARCH_BY_ID_RESPONSE_FIELD_DESCRIPTORS)
                         .build()
                 )
